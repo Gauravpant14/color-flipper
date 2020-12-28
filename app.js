13edacd4a97bb996d55cde1088db92f9565d70dc
Gaurav <aadeezcity@gmail.com>
2020-12-28 21:27:23 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const colors = ["green","red","rgba(133,122,200)","#f15025"];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+
+btn.addEventListener('click',function(){
+    //get random number bw 0-3 
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = color[randomNumber];
+});
+
+function getRandomNumber() {
+
+    return Math.floor( Math.random() * colors.length); //this will give random number bw 0-3
+    
+}
